d32043c61b135d1180ffeec2deee144157d47483
FlowerGolden <1327097302@qq.com>
2024-10-18 12:43:35 +0800

    feat: tree defs

--- MinimaxNode.cs
+++ MinimaxNode.cs
@@ -1,7 +1,20 @@
-﻿namespace MinimaxCS
+﻿using System.Collections.Generic;
+
+namespace MinimaxCS
 {
-    public class MinimaxNode
+#nullable enable
+    public class MinimaxNode<TData>
     {
-        
+        public MinimaxNode(TData data, MinimaxNode<TData>? parent)
+        {
+            Data = data;
+            Parent = parent;
+            Nodes = new List<MinimaxNode<TData>>();
+        }
+
+        public TData Data { get; }
+        public MinimaxNode<TData>? Parent { get; }
+        public List<MinimaxNode<TData>> Nodes { get; }
     }
+#nullable disable
 }
--- MinimaxTree.cs
+++ MinimaxTree.cs
@@ -1,7 +1,23 @@
-﻿namespace MinimaxCS
+﻿using System;
+
+namespace MinimaxCS
 {
-    public class MinimaxTree
+    public class MinimaxTree<TGame, TData> where TGame : IGame<TData>
     {
-        
+        public MinimaxTree(TGame game, TData data, int maxDepth)
+        {
+            Game = game;
+            MaxDepth = maxDepth;
+            Root = new MinimaxNode<TData>(data, null);
+        }
+
+        public TGame Game { get; }
+        public MinimaxNode<TData> Root { get; }
+        public int MaxDepth { get; }
+
+        public TData Run()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
