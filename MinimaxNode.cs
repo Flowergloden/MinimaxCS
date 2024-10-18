1e41416a142e4bdb9e80ab22620856ef3734b84a
FlowerGolden <1327097302@qq.com>
2024-10-18 13:38:01 +0800

    feat: add node value def & impl comparer

--- MinimaxNode.cs
+++ MinimaxNode.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MinimaxCS
 {
 #nullable enable
-    public class MinimaxNode<TData>
+    public class MinimaxNode<TData>: IComparable<MinimaxNode<TData>>
     {
         public MinimaxNode(TData data, MinimaxNode<TData>? parent)
         {
@@ -12,9 +13,17 @@ namespace MinimaxCS
             Nodes = new List<MinimaxNode<TData>>();
         }
 
+        public float Value { get; set; }
         public TData Data { get; }
         public MinimaxNode<TData>? Parent { get; }
         public List<MinimaxNode<TData>> Nodes { get; }
+
+        public int CompareTo(MinimaxNode<TData>? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (other is null) return 1;
+            return Value.CompareTo(other.Value);
+        }
     }
 #nullable disable
 }
